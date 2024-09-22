a95d399c0ca7e5bd3563b5df2f4610163dbc755b
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2024-09-22 12:16:54 -0300

    first commit init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "X Front End",
+  description: "Front end social media x",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
